da85e75773eeb542e17b932f9f700a4d4b4e1d38
omar2004712 <o.bahutair@gmail.com>
2022-10-28 20:53:48 +0400

    feat: built the StreamList component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-export default function StreamList() {
-  return <div>StreamList</div>;
+// eslint-disable-next-line no-shadow
+function StreamList({ fetchStreams, streams }) {
+  useEffect(() => {
+    fetchStreams();
+  }, []);
+
+  const renderList = () =>
+    // eslint-disable-next-line implicit-arrow-linebreak
+    streams.map((stream) => (
+      <div className="item" key={stream.id}>
+        <i className="large middle aligned icon camera" />
+        <div className="content">
+          {stream.title}
+          <div className="description">{stream.description}</div>
+        </div>
+      </div>
+    ));
+
+  return (
+    <div>
+      <h2>Streams</h2>
+      <div className="ui celled list">{renderList()}</div>
+    </div>
+  );
 }
+
+const mapStateToProps = (state) => ({ streams: Object.values(state.streams) });
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
