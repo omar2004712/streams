9e8daa210e692dde4c6046784780325e1979f89a
omar2004712 <o.bahutair@gmail.com>
2022-10-28 21:24:48 +0400

    feat: added the create stream button to the home page

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,16 +1,27 @@
+/* eslint-disable no-shadow */
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
-// eslint-disable-next-line no-shadow
-function StreamList({ fetchStreams, streams, currentUserId }) {
+// eslint-disable-next-line object-curly-newline
+function StreamList({ fetchStreams, streams, currentUserId, isSignedIn }) {
   useEffect(() => {
     fetchStreams();
   }, []);
 
   const renderAdmin = (stream) => {
     if (stream.userId === currentUserId) {
-      return <div>buttons</div>;
+      return (
+        <div className="right floated content">
+          <button type="button" className="ui button primary">
+            Edit
+          </button>
+          <button type="button" className="ui button negative">
+            Delete
+          </button>
+        </div>
+      );
     }
     return null;
   };
@@ -19,19 +30,34 @@ function StreamList({ fetchStreams, streams, currentUserId }) {
     // eslint-disable-next-line implicit-arrow-linebreak
     streams.map((stream) => (
       <div className="item" key={stream.id}>
+        {renderAdmin(stream)}
         <i className="large middle aligned icon camera" />
         <div className="content">
           {stream.title}
           <div className="description">{stream.description}</div>
         </div>
-        {renderAdmin(stream)}
       </div>
     ));
+
+  const renderCreate = () => {
+    if (isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   return (
     <div>
       <h2>Streams</h2>
       <div className="ui celled list">{renderList()}</div>
+      {renderCreate()}
     </div>
   );
 }
@@ -39,6 +65,7 @@ function StreamList({ fetchStreams, streams, currentUserId }) {
 const mapStateToProps = (state) => ({
   streams: Object.values(state.streams),
   currentUserId: state.auth.userId,
+  isSignedIn: state.auth.isSignedIn,
 });
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
