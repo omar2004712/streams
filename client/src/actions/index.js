e5c00c2926cf89352b748aeb2ac1a7b530d5fdfe
omar2004712 <o.bahutair@gmail.com>
2022-10-28 19:35:45 +0400

    fix: fixed the request in the editStream to await for the promise

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -32,7 +32,7 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const response = streams.put(`/streams/${id}`, formValues);
+  const response = await streams.put(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: (await response).data });
 };
