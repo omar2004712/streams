e2fe6599a8f40034dbe2d486641c9e076397715c
omar2004712 <o.bahutair@gmail.com>
2022-10-28 19:34:53 +0400

    feat: created a deleteStream action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -36,3 +36,9 @@ export const editStream = (id, formValues) => async (dispatch) => {
 
   dispatch({ type: EDIT_STREAM, payload: (await response).data });
 };
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
