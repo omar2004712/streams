e111a3022c352b38d3f90ddb31766a2d8f42bbdc
omar2004712 <o.bahutair@gmail.com>
2022-10-28 19:01:27 +0400

    feat: setup the server

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,17 +1,64 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-function StreamCreate() {
-  const renderInput = () => <div>I am an input</div>;
+function StreamCreate({ handleSubmit }) {
+  const renderError = (meta) => {
+    const { error, touched } = meta;
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+
+    return <div />;
+  };
+
+  const renderInput = ({ input, label, meta }) => (
+    <div className={`field ${meta.error && meta.touched ? 'error' : ''}`}>
+      <label>{label}</label>
+      <input type="text" {...input} />
+      {renderError(meta)}
+    </div>
+  );
+
+  const onSubmit = (formValues) => {
+    console.log(formValues);
+  };
 
   return (
-    <form>
-      <Field name="title" component={renderInput} />
-      <Field name="description" component={renderInput} />
+    <form onSubmit={handleSubmit(onSubmit)} className="ui form error">
+      <Field name="title" component={renderInput} label="Enter Title: " />
+      <Field
+        name="description"
+        component={renderInput}
+        label="Enter Description: "
+      />
+      <button type="submit" className="ui button primary">
+        Submit
+      </button>
     </form>
   );
 }
 
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = 'Title is required';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'Description is required';
+  }
+
+  return errors;
+};
+
 export default reduxForm({
   form: 'streamCreate',
+  validate,
 })(StreamCreate);
