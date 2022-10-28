6864f262045fd54b880c51ef01230c12e36af470
omar2004712 <o.bahutair@gmail.com>
2022-10-28 19:29:30 +0400

    feat: created a fetchStreams action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from './types';
 
 export const signIn = (userId) => ({ type: SIGN_IN, payload: { userId } });
 
@@ -9,4 +17,10 @@ export const createStream = (formValues) => async (dispatch) => {
   const response = await streams.post('/streams', formValues);
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.get('/streams');
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
