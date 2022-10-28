0cc254ce3afa93b025ac65e899098285a6872bc4
omar2004712 <o.bahutair@gmail.com>
2022-10-28 19:33:21 +0400

    feat: create an editStream action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -30,3 +30,9 @@ export const fetchStream = (id) => async (dispatch) => {
 
   dispatch({ type: FETCH_STREAM, payload: response.data });
 };
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: (await response).data });
+};
