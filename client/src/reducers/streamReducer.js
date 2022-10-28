a64bcced2e4f9198ffdad8070a1fb3cb967d3df0
omar2004712 <o.bahutair@gmail.com>
2022-10-28 20:02:22 +0400

    feat: created a reducer for EDIT_STREAM action

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -11,6 +11,7 @@ export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAM:
     case CREATE_STREAM:
+    case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     default:
       return state;
