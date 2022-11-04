a6d4038f023a86eb7aaf6de9f4faf8c1b2f5afc9
omar2004712 <o.bahutair@gmail.com>
2022-11-04 21:22:34 +0400

    fix: if the user goes directly to the StreamEdit page the streams object in the redux store will be empty and the stream we want to load on the page will be undefined
    to fix it we check if the stream is found in the streams object
    if not then fetch it using the fetchStream action creator

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-function StreamEdit({ stream }) {
-  console.log('stream: ', stream);
-  return <div>StreamEdit</div>;
+function StreamEdit({ match, fetchStream, stream }) {
+  useEffect(() => {
+    if (!stream) {
+      // if the stream already exists in the streams object in the redux store avoid fetching it from the server
+      fetchStream(match.params.id);
+    }
+  }, []);
+
+  return <div>{stream ? stream.title : 'Loading'}</div>;
 }
 
 const mapStateToProps = (state, props) => ({
   stream: state.streams[props.match.params.id],
 });
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
