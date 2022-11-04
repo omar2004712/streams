1825d476d5a50fb412960b1df2cf43d2e6b4e77d
omar2004712 <o.bahutair@gmail.com>
2022-11-04 22:29:24 +0400

    fix: fixed the http verb and used patch instead of put

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -35,7 +35,7 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues, history) => async (dispatch) => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: (await response).data });
   history.push('/');
