c75c863578631b75a206b1e7b244a603741faea7
omar2004712 <o.bahutair@gmail.com>
2022-11-04 21:44:02 +0400

    refactor: refactored the StreamCreate and StreamEdit common login into seperate component call StreamForm

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,35 +1,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { createStream } from '../../actions/index';
-
-// to avoid redefining the function every time the component rerenders
-const renderInput = ({ input, label, meta }) => (
-  <div className={`field ${meta.error && meta.touched ? 'error' : ''}`}>
-    <label>{label}</label>
-    <input type="text" {...input} />
-    {renderError(meta)}
-  </div>
-);
-
-const renderError = (meta) => {
-  const { error, touched } = meta;
-  if (touched && error) {
-    return (
-      <div className="ui error message">
-        <div className="header">{error}</div>
-      </div>
-    );
-  }
-
-  return <div />;
-};
+import StreamForm from './StreamForm';
 
 // eslint-disable-next-line no-shadow
-function StreamCreate({ handleSubmit, createStream }) {
+function StreamCreate({ createStream }) {
   const history = useHistory();
 
   const onSubmit = (formValues) => {
@@ -37,37 +15,11 @@ function StreamCreate({ handleSubmit, createStream }) {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="ui form error">
-      <Field name="title" component={renderInput} label="Enter Title: " />
-      <Field
-        name="description"
-        component={renderInput}
-        label="Enter Description: "
-      />
-      <button type="submit" className="ui button primary">
-        Submit
-      </button>
-    </form>
+    <div>
+      <h3>Create Stream</h3>
+      <StreamForm onSubmit={onSubmit} />
+    </div>
   );
 }
 
-const validate = (formValues) => {
-  const errors = {};
-
-  if (!formValues.title) {
-    errors.title = 'Title is required';
-  }
-
-  if (!formValues.description) {
-    errors.description = 'Description is required';
-  }
-
-  return errors;
-};
-
-const formWrapped = reduxForm({
-  form: 'streamCreate',
-  validate,
-})(StreamCreate);
-
-export default connect(null, { createStream })(formWrapped);
+export default connect(null, { createStream })(StreamCreate);
---
+++ client/src/components/streams/StreamForm.js
@@ -0,0 +1,63 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { useHistory } from 'react-router-dom';
+
+// to avoid redefining the function every time the component rerenders
+const renderInput = ({ input, label, meta }) => (
+  <div className={`field ${meta.error && meta.touched ? 'error' : ''}`}>
+    <label>{label}</label>
+    <input type="text" {...input} />
+    {renderError(meta)}
+  </div>
+);
+
+const renderError = (meta) => {
+  const { error, touched } = meta;
+  if (touched && error) {
+    return (
+      <div className="ui error message">
+        <div className="header">{error}</div>
+      </div>
+    );
+  }
+
+  return <div />;
+};
+
+// eslint-disable-next-line no-shadow
+function StreamForm({ onSubmit, handleSubmit }) {
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="ui form error">
+      <Field name="title" component={renderInput} label="Enter Title: " />
+      <Field
+        name="description"
+        component={renderInput}
+        label="Enter Description: "
+      />
+      <button type="submit" className="ui button primary">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = 'Title is required';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'Description is required';
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: 'streamForm',
+  validate,
+})(StreamForm);
