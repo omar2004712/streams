e3450a762480f28c8937afce34d7d9f94f118e0b
omar2004712 <o.bahutair@gmail.com>
2022-11-11 23:28:37 +0400

    feat(client): display info about the stream in the StreamShow component

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-export default function StreamShow() {
-  return <div>StreamShow</div>;
+function StreamShow({ match, fetchStream, stream }) {
+  useEffect(() => {
+    if (!stream) {
+      fetchStream(match.params.id);
+    }
+  }, []);
+
+  const renderStream = () => {
+    if (!stream) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h1>{stream.title}</h1>
+        <h5>{stream.description}</h5>
+      </div>
+    );
+  };
+
+  return <div>{renderStream()}</div>;
 }
+
+const mapStateToProps = (state, props) => ({
+  stream: state.streams[props.match.params.id],
+});
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
