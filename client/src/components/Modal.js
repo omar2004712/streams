304f15b642bce3351daa5dbd29c856115bdd7471
omar2004712 <o.bahutair@gmail.com>
2022-11-11 21:39:09 +0400

    feat(client): created the Modal component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function Modal({ title, content, actions, onDismiss }) {
+  return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible active" onClick={onDismiss}>
+      <div
+        className="ui standard modal visible active"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="header">{title}</div>
+        <div className="content">{content}</div>
+        <div className="actions">{actions}</div>
+      </div>
+    </div>,
+    document.querySelector('#modal')
+  );
+}
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Modal from '../Modal';
 
 export default function StreamDelete() {
-  return <div>StreamDelete</div>;
+  const history = useHistory();
+
+  const actions = (
+    <>
+      <button className="ui negative button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
+
+  return (
+    <div>
+      StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push('/')}
+      />
+    </div>
+  );
 }
