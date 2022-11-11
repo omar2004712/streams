b022284a5dce78eeb12db5bb0b5eba1080229357
omar2004712 <o.bahutair@gmail.com>
2022-11-11 22:46:28 +0400

    fix(client): fixed the bug with the /streams/new route
    it was showing both the StreamCreate and StreamShow components
    
    when going to /streams/new the new is considered is an id in the Route /streams/id
    which cause the StreamShow component to render
    to fix this bug the Switch component was used

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
@@ -13,11 +13,13 @@ export default function App() {
       <BrowserRouter>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/streams/delete/:id" exact component={StreamDelete} />
-          <Route path="/streams/:id" exact component={StreamShow} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
       </BrowserRouter>
     </div>
